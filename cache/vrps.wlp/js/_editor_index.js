979f624283ce47f41fbd8424f7aeb2152d41140a
zacharyellison <zachary@zacharyellison.com>
2025-02-16 11:25:32 -0500

    wunderland packaging

---
+++ cache/vrps.wlp/js/_editor_index.js
@@ -0,0 +1,4 @@
+import * as __wonderlandengine_components from '@wonderlandengine/components';
+_registerEditor(__wonderlandengine_components);
+import * as _C__Users_zachr_code_vrps_vrps_js_button_js from 'C:/Users/zachr/code/vrps/vrps/js/button.js';
+_registerEditor(_C__Users_zachr_code_vrps_vrps_js_button_js);
